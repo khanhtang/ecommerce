3e733d11701defc8e0f83628c4c153bef7888ccc
Khanhtn <khanhtang1403@gmail.com>
2022-11-17 09:06:52 +0700

    Rspec model product

--- app/controllers/admin/xlsx_controller.rb
+++ app/controllers/admin/xlsx_controller.rb
@@ -1,7 +1,7 @@
 class Admin::XlsxController < Admin::BaseController
   def index
-    @orders = Order.this_week.group(:user_id).count
-    @order_details = OrderDetail.this_week.group(:product_id).count
+    @orders = Order.this_week.group(:user_id).includes(:users).count
+    @order_details = OrderDetail.this_week.group(:product_id).includes(:products).count
     respond_to do |format|
       format.html
       format.xlsx do
